a3c19ee328cae463fbfc2dcf515daa22029b4233
jose <nextgenappsllc@gmail.com>
2017-02-20 02:01:07 -0500

    removed Package.swift

--- Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-
-let package = Package(
-    name: "NGAFramework",
-    dependencies: [
-        .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0)
-    ]
-)
